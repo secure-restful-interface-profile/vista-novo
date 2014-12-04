04c8086e7c44c88290e19f8d4806157b7328536e
Dave Hill <dwhill@mitre.org>
2014-12-04 06:28:04 -0500

    Added debug logging capability.  Added debug statements.

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+var fs = require('fs');
+var util = require('util');
+var log_file = fs.createWriteStream(__dirname + '/log/debug.log', {flags : 'w'});
+var log_stdout = process.stdout;
+
+console.log = function(d) { //
+  log_file.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};
+
+// var log = bunyan.createLogger({
+// 	name: 'access_log',
+// 	streams: [{
+//     path: __dirname + 'access_log',
+//     level: bunyan.DEBUG
+// 	}]
+// });
+
 var vistaNovo = require(__dirname + '/config/express');
 vistaNovo.app.listen(3000);
 console.log('Listening on port 3000');
